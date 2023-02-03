b87135f5746157c6eaa4585a30e3244a9af2bf11
shenyong <15826383768@163.com>
2023-02-03 17:25:03 +0800

    配置添加

---
+++ .eslintrc.js
@@ -0,0 +1,74 @@
+// @ts-check
+const { defineConfig } = require('eslint-define-config')
+module.exports = defineConfig({
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    jsxPragma: 'React',
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  extends: [
+    'plugin:vue/vue3-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended'
+  ],
+  rules: {
+    'vue/script-setup-uses-vars': 'off',
+    'vue/no-reserved-component-names': 'off',
+    '@typescript-eslint/ban-ts-ignore': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'vue/custom-event-name-casing': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
+    '@typescript-eslint/ban-types': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'no-unused-vars': 'off',
+    // 要求或禁止函数圆括号之前有一个空格
+    'space-before-function-paren': [2, {
+      anonymous: 'always',
+      named: 'never',
+      asyncArrow: 'always'
+    }],
+    // 要求组件名（文件名）必须是多单词的
+    'vue/multi-word-component-names': 0,
+
+    'prettier/prettier': 'off',
+     
+    'vue/attributes-order': 'off',
+    'vue/one-component-per-file': 'off',
+    'vue/html-closing-bracket-newline': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    'vue/attribute-hyphenation': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/require-explicit-emits': 'off',
+    'generator-star-spacing': 'off',
+    // "vue/html-self-closing": ["off",{
+    //   "html": {
+    //     "void": "never",
+    //     "normal": "any",
+    //     "component": "any"
+    //   },
+    //   "svg": "always",
+    //   "math": "always"
+    // }]
+  }
+})
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import './style.css'
+
+
+// 引入element-plus
+import { setupElementPlus } from '@/plugins/elementPlus'
+
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// 创建实例
+const setupAll = async () => {
+    const app = createApp(App)
+
+    setupElementPlus(app)
+
+    app.mount('#app')
+}
+
+setupAll()
+
