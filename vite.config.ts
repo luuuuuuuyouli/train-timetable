0aeefb23dae1def64a27b26de0b5836c0d755310
shenyong <15826383768@163.com>
2023-02-03 11:50:54 +0800

    1

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig(({ command }: ConfigEnv) => {
   return {
-    base: './',
+    base: '/train-timetable/',
     plugins: [
       vue(),
     ],
